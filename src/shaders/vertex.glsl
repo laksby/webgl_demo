946a1ce7c2bb076002ac3510877b84e24467d15b
laksby <laksby@icloud.com>
2023-11-12 22:33:19 +0100

    Adjusted shaders

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -15,7 +15,7 @@ void main() {
 
   highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
   highp vec3 directionalLightColor = vec3(1, 1, 1);
-  highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
+  highp vec3 directionalVector = normalize(vec3(0.5, 0.5, 0.5));
 
   highp vec4 transformedNormal = normalMatrix * vec4(normalAttribute, 1.0);
   highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
